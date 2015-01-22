7a8c12f5e60e2ba75e9907a09d78f184754b9e24
Ronnie Nyaga <rnafundi@gmail.com>
2015-01-22 18:29:15 +0300

    Finishing up

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -4,6 +4,22 @@ class HousesController < ApplicationController
   
   def index
     @houses = House.all
+    @total = 0.0
+    @houses.each {|house| @total += house.price }
+    
+    @houses.each {|house|
+      numberOfBedRooms = house.numberOfBedRooms
+      numberOfBathRooms = house.numberOfBathRooms
+      numberOfReceptionRooms = house.numberOfReceptionRooms
+      size = house.size
+      security = house.security
+      
+      price = cost_of_the_house(numberOfBedRooms, numberOfBathRooms, numberOfReceptionRooms, size, security)
+      house.price = price
+      house.save!
+    }
+    
+
   end
 
   def new
@@ -15,7 +31,7 @@ class HousesController < ApplicationController
 
   def create
     @house = House.new(house_params)
-
+    
     respond_to do |format|
       if @house.save
         format.html { redirect_to root_url, notice: 'House was successfully created.' }
@@ -60,4 +76,68 @@ class HousesController < ApplicationController
   def house_params
     params.require(:house).permit(:location, :numberOfBedRooms, :numberOfBathRooms, :numberOfReceptionRooms, :size, :security, :photo)
   end
+  
+  def cost_of_the_house(numberOfBedRooms, numberOfBathRooms, numberOfReceptionRooms, size, security)
+    #calculate price for numberOfBedRooms
+    @price = 0.0
+    numberOfBedRooms = numberOfBedRooms.to_i
+    numberOfBathRooms = numberOfBathRooms.to_i
+    numberOfReceptionRooms = numberOfReceptionRooms.to_i
+    size = size.to_i
+    if numberOfBedRooms < 5
+      #code
+      @price = numberOfBedRooms * 3_000_000
+    end
+    if numberOfBedRooms > 4
+      #code
+      @price = 4 * 3_000_000
+      numberOfBedRooms = numberOfBedRooms - 4
+      @price += (numberOfBedRooms * 1_200_000)
+    end
+    
+    puts @price
+    
+    #factor in cost from numberOfBathRooms
+    @price += (@price * 0.05 * numberOfBathRooms)
+    
+    puts @price
+    
+    #factor in cost from numberOfReceptionRooms
+    #error in question. confirm
+    @price += (@price * (1.2 + (numberOfReceptionRooms - 1)**numberOfBedRooms))
+    
+    puts @price
+    
+    #factor in cost from size
+    if size < 1_000
+      #code
+      difference = 1_000.0 - size
+      cost_of_depreciation = difference/5.0 * 0.01
+      @price -= cost_of_depreciation
+    end
+    if size > 1_000
+      #code
+      difference = size - 1_000.0
+      cost_of_appreciation = difference/10.0 * 0.01
+      @price += cost_of_appreciation
+    end
+    
+    if security.downcase == "low"
+      #code
+      security_cost = @price * 0.2
+      @price -= security_cost
+    end
+    if security.downcase == "medium"
+      #code
+    end
+    if security.downcase == "high"
+      #code
+      security_cost = @price * 0.2
+      @price += security_cost
+    end
+    
+    puts @price
+    @price
+  end
+
 end

fe72ea2b5adb378e188fcd70f6d6d0bc02c8df37
Ronnie Nyaga <rnafundi@gmail.com>
2015-01-23 11:44:32 +0300

    Tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,12 @@ Rails.application.routes.draw do
   get 'pages/about'
 
   get 'pages/contacts'
+  
+  get 'landlords/sign_in'
+  
+  get 'houses/new'
+  
+  get 'houses/edit'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
